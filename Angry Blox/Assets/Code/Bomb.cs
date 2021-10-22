b5449c9619e440244c71eef31f9a2c06f6e60af2
HaoOneBin <37146238+HaoOneBin@users.noreply.github.com>
2021-10-21 23:21:33 -0500

    Finish Angry Blox

---
+++ Angry Blox/Assets/Code/Bomb.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public class Bomb : MonoBehaviour {
+    public float ThresholdForce = 2;
+    public GameObject ExplosionPrefab;
+
+    private void Destruct()
+    {
+        Destroy(gameObject);
+    }
+
+    private void Boom()
+    {
+        GetComponent<PointEffector2D>().enabled = true;
+        GetComponent<SpriteRenderer>().enabled = false;
+        
+        // make it be a child of the box’s parent, so that
+        // when the box goes away, it doesn’t take the explosion with it.
+        Instantiate(ExplosionPrefab, transform.position, Quaternion.identity, transform.parent);
+
+        Invoke("Destruct", 0.1f);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (GetComponent<Rigidbody2D>().velocity.magnitude >= ThresholdForce)
+        {
+            Boom();
+        }
+    }
+}
---
+++ Angry Blox/Assets/Code/TargetBox.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TargetBox : MonoBehaviour
+{
+    /// <summary>
+    /// Targets that move past this point score automatically.
+    /// </summary>
+    public static float OffScreen;
+
+    SpriteRenderer box_SpriteRenderer;
+    
+    private Rigidbody2D box_rigidbody2D;
+
+    private bool isAdded = false;
+
+    internal void Start() {
+        OffScreen = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width-100, 0, 0)).x;
+        box_SpriteRenderer = GetComponent<SpriteRenderer>();
+        box_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    internal void Update()
+    {
+        if (transform.position.x > OffScreen)
+            Scored();
+    }
+
+    private void Scored()
+    {
+        if (!isAdded)
+        {
+            isAdded = true;
+            // Turn target box green    
+            box_SpriteRenderer.color = Color.green;
+            // Add score of mass
+            ScoreKeeper.AddToScore(box_rigidbody2D.mass);
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name != "Catapult Base")
+        {
+            if (collision.collider.tag == "Ground")
+            {
+                Scored();
+            }
+        }
+
+    }
+}
