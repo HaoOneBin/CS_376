f99bbb5107b8f25cd1f6ea8f80054543c497ca82
HaoOneBin <37146238+HaoOneBin@users.noreply.github.com>
2021-11-14 19:43:52 -0600

    Update HW6 Pool Game documents and sound

--- Pool Game/Assets/Scripts/Player.cs
+++ Pool Game/Assets/Scripts/Player.cs
@@ -6,15 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    private AudioSource myAudioSource;
+    public AudioClip hit;
+
     Vector2 mouseStartPos, mouseEndPos;
     Rigidbody2D playerRB2D;
     public float force = 2;
     public Vector2 minForce, maxForce;
-    public float moveSpeed = 5;
+    public float moveSpeed = 5;    
 
     void Start()
     {
         playerRB2D = GetComponent<Rigidbody2D>();
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
@@ -22,6 +26,20 @@ public class Player : MonoBehaviour
         var horizontal = Input.GetAxisRaw("Horizontal");
         var vertical = Input.GetAxisRaw("Vertical");
         playerRB2D.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name == "Ball_1" ||
+            collision.collider.name == "Ball_2" ||
+            collision.collider.name == "Ball_3" ||
+            collision.collider.name == "Ball_4" ||
+            collision.collider.name == "Ball_5" ||
+            collision.collider.name == "Ball_6")
+        {
+            myAudioSource.PlayOneShot(hit);
+        }
 
     }
 }
